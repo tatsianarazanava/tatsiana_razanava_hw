94ba5f198e5776ca879d231df989ee9752e79a63
tatsianarazanava <tatsiana_razanava@epam.com>
2017-12-21 20:23:33 +0300

    task 1 reformating

--- src/com/epam/lecture1/Task1.java
+++ src/com/epam/lecture1/Task1.java
@@ -2,26 +2,26 @@ package com.epam.lecture1;
 
 public class Task1 {
 
-    public static void main (String args []) {
+    public static void main(String args[]) {
         String str1 = "We make work HUMAN.";
         String str2 = "Unlock full potential. Celebrate positive moments.";
         String str3 = "Every day.";
         //task 2a
 
-        System.out.println (str1 + "\n");
-        System.out.println (str2 );
-        System.out.println (str3);
+        System.out.println(str1 + "\n");
+        System.out.println(str2);
+        System.out.println(str3);
         System.out.println();
 
-        System.out.print("2nd string lenght is: " );
+        System.out.print("2nd string lenght is: ");
         System.out.println(str2.length());
         //task 2b
 
         System.out.print("\n");
-        System.out.print ("2nd string in lower case looks like: " + str2.toLowerCase());
+        System.out.print("2nd string in lower case looks like: " + str2.toLowerCase());
         //task 2c
 
-        System.out.print("\n");
+        System.out.print("\n\n");
         System.out.print(str2.replace('e', 'E'));
         //task 2d
 
